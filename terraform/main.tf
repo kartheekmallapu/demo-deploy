1c4bf1b313149987c436586e64d19e1bf61f9125
Tze Liang <tze@aiyor.net>
2020-05-04 06:37:21 +0000

    Update terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,16 @@ provider "aws" {
   }
 }
 
+
+provider "aws" {
+  alias  = "apac_prd"
+  region = "ap-southeast-2"
+
+  assume_role {
+    role_arn = "arn:aws:iam::052578022106:role/orica-vault-poweruser-role"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket         = "orica-global-prd-terraform"
@@ -53,3 +63,27 @@ module "soe02" {
   }
 }
 
+
+module "soe01" {
+  providers = {
+    aws = aws.apac_prd
+  }
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  source               = "git@gitlab.com:orica/terraform-modules/aws-orica-ec2.git"
+  host_name            = "support.test.shd"
+  instance_type        = "t3a.medium"
+  is_soe               = "true"
+  os                   = "windows-2016"
+  subnet_name          = "PRD-CETST-AZ1-SN"
+  security_group_names = ["TST-CE-CUTOVER-SGT"]
+  key_name             = "tf-keypair"
+  description          = "VM Description"
+
+  tags = {
+    "tag2" = "eg2"
+    "tag3" = "eg3"
+  }
+}
